b3d3ff2247218ff3876c518b14504829813c14cd
resul <mail.resulcelik@gmail.com>
2026-04-12 19:06:04 +0300

    refactor: restructure order receipt template for improved readability and maintainability

--- woocommerce/checkout/order-receipt.php
+++ woocommerce/checkout/order-receipt.php
@@ -20,32 +20,33 @@ if (! defined('ABSPATH')) {
 	exit;
 }
 ?>
-<div class="card-container card-container-secondary w-full">
-	<h2 class="card-title">
-		<?php esc_html_e('Order details', 'woocommerce'); ?>
-	</h2>
-	<ul class="order_details w-full flex flex-col gap-20">
-		<li class="order w-full flex fleex-row items-center justify-between">
-			<?php esc_html_e('Order number:', 'woocommerce'); ?>
-			<strong><?php echo esc_html($order->get_order_number()); ?></strong>
-		</li>
-		<li class="date w-full flex fleex-row items-center justify-between">
-			<?php esc_html_e('Date:', 'woocommerce'); ?>
-			<strong><?php echo esc_html(wc_format_datetime($order->get_date_created())); ?></strong>
-		</li>
-		<li class="total w-full flex fleex-row items-center justify-between">
-			<?php esc_html_e('Total:', 'woocommerce'); ?>
-			<strong><?php echo wp_kses_post($order->get_formatted_order_total()); ?></strong>
-		</li>
-		<?php if ($order->get_payment_method_title()) : ?>
-			<li class="method w-full flex fleex-row items-center justify-between">
-				<?php esc_html_e('Payment method:', 'woocommerce'); ?>
-				<strong><?php echo wp_kses_post($order->get_payment_method_title()); ?></strong>
-			</li>
-		<?php endif; ?>
-	</ul>
-</div>
+
 <div class="woocommerce-order w-full max-w-[540px] flex flex-col items-start justify-start gap-10 px-20 py-60">
+	<div class="card-container card-container-secondary w-full">
+		<h2 class="card-title">
+			<?php esc_html_e('Order details', 'woocommerce'); ?>
+		</h2>
+		<ul class="order_details w-full flex flex-col gap-20">
+			<li class="order w-full flex fleex-row items-center justify-between">
+				<?php esc_html_e('Order number:', 'woocommerce'); ?>
+				<strong><?php echo esc_html($order->get_order_number()); ?></strong>
+			</li>
+			<li class="date w-full flex fleex-row items-center justify-between">
+				<?php esc_html_e('Date:', 'woocommerce'); ?>
+				<strong><?php echo esc_html(wc_format_datetime($order->get_date_created())); ?></strong>
+			</li>
+			<li class="total w-full flex fleex-row items-center justify-between">
+				<?php esc_html_e('Total:', 'woocommerce'); ?>
+				<strong><?php echo wp_kses_post($order->get_formatted_order_total()); ?></strong>
+			</li>
+			<?php if ($order->get_payment_method_title()) : ?>
+				<li class="method w-full flex fleex-row items-center justify-between">
+					<?php esc_html_e('Payment method:', 'woocommerce'); ?>
+					<strong><?php echo wp_kses_post($order->get_payment_method_title()); ?></strong>
+				</li>
+			<?php endif; ?>
+		</ul>
+	</div>
 	<h2 class="w-full text-center paragraph-2xl paragraph-medium text-gray-900 pb-30 ">Ödeme Yöntemi</h2>
 </div>
 <?php do_action('woocommerce_receipt_' . $order->get_payment_method(), $order->get_id()); ?>
